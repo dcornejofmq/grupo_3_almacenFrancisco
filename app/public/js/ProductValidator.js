ca25c9d1f1060cc3745ca23e6b6236445fba399a
CrisStefani <stefani.cristian@yahoo.com.ar>
2021-05-14 11:09:35 -0300

    validacion

--- app/public/js/ProductValidator.js
+++ app/public/js/ProductValidator.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('#product');
 
 form.addEventListener('submit', function(event){
-    event.preventDefault();
+    
     
     const productField = this.querySelector('#nameProd');
     const descriptionField = this.querySelector('#description');
@@ -12,18 +12,24 @@ form.addEventListener('submit', function(event){
     
     if(productField.value == ''){
         alert('Debes completar el campo Nombre del Producto');
+        event.preventDefault();
         }else if(productField.value.length <5){
-        alert('El nombre del producto debe tener mas de 5 caracteres')
+        alert('El nombre del producto debe tener mas de 5 caracteres');
+        event.preventDefault();
         }
        if(descriptionField.value == ''){
            alert('Debes completar la descipcion');
+           event.preventDefault();
         }else if(descriptionField.value.length <19){
-            alert('La descripcion debe tener mas de 20 caracteres')
+            alert('La descripcion debe tener mas de 20 caracteres');
+            event.preventDefault();
         } 
         if(!allowedExtensions.exec(filePath)){
         alert('Adjunta una imagen con las siguientes extensiones .jpeg/.jpg/.png/.gif');
+        event.preventDefault();
         fileInput.value = '';
         return false;
+        
         }
 });
 
